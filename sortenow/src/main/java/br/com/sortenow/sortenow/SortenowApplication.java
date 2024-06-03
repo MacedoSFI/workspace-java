797adb263f7b9b76780f38c1fa51113459161920
Felipe Macedo <felipe.nomade@yahoo.com>
2024-06-03 15:52:32 -0300

    realizado config email e passw

---
+++ sortenow/src/main/java/br/com/sortenow/sortenow/SortenowApplication.java
@@ -0,0 +1,13 @@
+package br.com.sortenow.sortenow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SortenowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SortenowApplication.class, args);
+	}
+
+}
---
+++ sortenow/src/test/java/br/com/sortenow/sortenow/SortenowApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sortenow.sortenow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SortenowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
